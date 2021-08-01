6ff4160e8e2b119cd2091b02f2da67eefa1e3f85
arjunkotari <arjun.kotari123@gmail.com>
2021-08-01 19:40:38 +0530

    Added:User name starts with Capital and has minimum 3 characters

--- src/main/java/userRegistration/UserRegistration.java
+++ src/main/java/userRegistration/UserRegistration.java
@@ -1,7 +1,12 @@
 package userRegistration;
 
+import java.util.regex.Pattern;
+
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration....");
+        //First letter mush be in capital and remaining letters must be 2 or more characters
+        System.out.println(Pattern.matches("[A-Z]{1}[a-zA-Z0-9]{2,}","Arjun")); //true
+        System.out.println(Pattern.matches("[A-Z]{1}[a-zA-Z0-9]{2,}","arjun")); //false
     }
 }
