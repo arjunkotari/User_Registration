06496e1d761b76124358ebb0662396fae05d4ae9
arjunkotari <arjun.kotari123@gmail.com>
2021-08-01 19:46:13 +0530

    Added:Last name starts with Capital and has minimum 3 characters

--- src/main/java/userRegistration/UserRegistration.java
+++ src/main/java/userRegistration/UserRegistration.java
@@ -3,10 +3,22 @@ package userRegistration;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+    /*
+    User Registration Form.
+     */
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration....");
-        //First letter mush be in capital and remaining letters must be 2 or more characters
+        /*
+        First Name
+        First letter mush be in capital and remaining letters must be 2 or more characters
+         */
         System.out.println(Pattern.matches("[A-Z]{1}[a-zA-Z0-9]{2,}","Arjun")); //true
         System.out.println(Pattern.matches("[A-Z]{1}[a-zA-Z0-9]{2,}","arjun")); //false
+        /*
+        Last Name
+        First letter mush be in capital and remaining letters must be 2 or more characters
+         */
+        System.out.println(Pattern.matches("[A-Z]{1}[a-zA-Z0-9]{2,}","Sai7")); //true
+        System.out.println(Pattern.matches("[A-Z]{1}[a-zA-Z0-9]{2,}","sai")); //false
     }
 }
