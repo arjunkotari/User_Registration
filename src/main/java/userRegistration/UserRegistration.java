c5b4fdfbd67d37d93dff8dc5b7e65a8c5d2e0de6
arjunkotari <arjun.kotari123@gmail.com>
2021-08-04 14:39:57 +0530

    Added:Email

--- src/main/java/userRegistration/UserRegistration.java
+++ src/main/java/userRegistration/UserRegistration.java
@@ -22,19 +22,17 @@ public class UserRegistration {
         System.out.println(Pattern.matches("[A-Z]{1}[a-zA-Z0-9]{2,}","sai")); //false
 
         /*
+        Email
+         */
+        System.out.println(Pattern.matches("^([a-z0-9]+.)+@([a-z0-9]+.)([a-z]+.)[a-z]{2,3}+$","abc.100@yahoo.com")); //true
+        System.out.println(Pattern.matches("^([a-z0-9]+.)+@([a-z0-9]+.)([a-z]+.)[a-z]{2,3}+$","abc123@.com.com")); //false
+
+        /*
         Mobile Number
         country code and 10 digit number
          */
         System.out.println(Pattern.matches("[0-9]{2}[ ]{1}[0-9]{10}","91 8400998800")); //true
         System.out.println(Pattern.matches("[0-9]{2}[ ]{1}[0-9]{10}","8400998800")); //false
-
-        /*
-        Password
-        It contain minimum 8 characters.
-        It contain at least 1 upper case
-         */
-        System.out.println(Pattern.matches("^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}","asdfA5kad")); //true
-        System.out.println(Pattern.matches("^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}","asdAdrGh")); //false
 
     }
 }
